c0d986d246d33ea231164357b93f0d6bb20b3729
Jamie McNeil <jamievmcneil@gmail.com>
2025-04-14 15:40:21 -0400

    Install react-native-maps; Get apple map rendering

---
+++ app.config.js
@@ -0,0 +1,46 @@
+import 'dotenv/config';
+
+export default {
+  expo: {
+    name: 'BikeNav',
+    slug: 'BikeNav',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/images/icon.png',
+    scheme: 'myapp',
+    userInterfaceStyle: 'automatic',
+    newArchEnabled: true,
+    ios: {
+      config: {
+        googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY
+      },
+      supportsTablet: true
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/images/adaptive-icon.png',
+        backgroundColor: '#ffffff'
+      }
+    },
+    web: {
+      bundler: 'metro',
+      output: 'static',
+      favicon: './assets/images/favicon.png'
+    },
+    plugins: [
+      'expo-router',
+      [
+        'expo-splash-screen',
+        {
+          image: './assets/images/splash-icon.png',
+          imageWidth: 200,
+          resizeMode: 'contain',
+          backgroundColor: '#ffffff'
+        }
+      ]
+    ],
+    experiments: {
+      typedRoutes: true
+    }
+  }
+};
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,54 @@
-import { Text, View } from "react-native";
+import React from 'react';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function Index() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        justifyContent: 'center',
+        alignItems: 'center'
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      {/* <Text>Hello Friend</Text> */}
+      <MapView
+        // provider={PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        }}
+        style={{ width: '100%', height: '100%' }}
+      />
     </View>
   );
 }
+
+// export default function Index() {
+//   return (
+//     <View style={styles.container}>
+//       <MapView
+//         provider={PROVIDER_GOOGLE}
+//         initialRegion={{
+//           latitude: 37.78825,
+//           longitude: -122.4324,
+//           latitudeDelta: 0.0922,
+//           longitudeDelta: 0.0421
+//         }}
+//         style={styles.map}
+//       />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1
+//   },
+//   map: {
+//     width: '100%',
+//     height: '100%'
+//   }
+// });
